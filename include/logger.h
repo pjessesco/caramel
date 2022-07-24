476d27927882492b98f90af6d8e6fea461cf9795
Jino Park <pjessesco@gmail.com>
2022-07-24 23:18:31 +0900

    Add logger & image class

---
+++ include/logger.h
@@ -0,0 +1,109 @@
+//
+// This software is released under the MIT license.
+//
+// Copyright (c) 2022 Jino Park
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <filesystem>
+
+namespace Caramel {
+
+    class Logger {
+    public:
+        inline static void print_log(const std::string &file,
+                                     int line,
+                                     const std::string &msg) {
+            std::cout << log_prefix() << "In " << get_filename(file) << " " << line << ": " << msg << std::endl;
+        }
+
+        inline static void print_err(const std::string &file,
+                                     int line,
+                                     const std::string &msg) {
+            std::cout << err_prefix() << "In " << get_filename(file) << " " << line << ": " << msg << std::endl;
+        }
+
+        inline static void print_warn(const std::string &file,
+                                      int line,
+                                      const std::string &msg) {
+            std::cout << warn_prefix() << "In " << get_filename(file) << " " << line << ": " << msg << std::endl;
+        }
+
+    private:
+        inline static std::string current_time_string() {
+            std::time_t now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+
+            auto current_time = std::localtime(&now);
+            std::string hour = std::to_string(current_time->tm_hour);
+            std::string min = std::to_string(current_time->tm_min);
+            std::string sec = std::to_string(current_time->tm_sec);
+
+            return "[" + hour + ":" + min + ":" + sec + "]";
+        }
+
+        inline static std::string log_prefix() {
+            return current_time_string() + "\033[1;30m[LOG ]\033[0m ";
+        }
+
+        inline static std::string err_prefix() {
+            return current_time_string() + "\033[1;31m[ERR ]\033[0m ";
+        }
+        inline static std::string warn_prefix() {
+            return current_time_string() + "\033[1;31m[WARN]\033[0m ";
+        }
+
+        inline static std::string get_filename(const std::string &str) {
+            std::filesystem::path path(str);
+            return (path.parent_path().filename() / path.filename()).string();
+        }
+    };
+
+    enum class LogType {
+        LOG,
+        ERR,
+        WARN
+    };
+}
+
+#define MESSAGE(type, msg) do {                                                    \
+    switch(type) {                                                                 \
+        case LogType::LOG:                                                         \
+            Logger::print_log(__FILE__, __LINE__, msg);                            \
+            break;                                                                 \
+        case LogType::ERR:                                                         \
+            Logger::print_err(__FILE__, __LINE__, msg);                            \
+            throw std::runtime_error("error");                                     \
+            break;                                                                 \
+        case LogType::WARN:                                                        \
+            Logger::print_warn(__FILE__, __LINE__, msg);                           \
+            break;                                                                 \
+        default:                                                                   \
+            break;                                                                 \
+    }                                                                              \
+} while(0);
+
+#define NOT_IMPLEMENTED() do{         \
+    MESSAGE(LogType::ERR, "NOT IMPLEMENTED") \
+} while(0);
+
---
+++ src/image.cpp
@@ -0,0 +1,105 @@
+//
+// This software is released under the MIT license.
+//
+// Copyright (c) 2022 Jino Park
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+#include <common.h>
+#include <image.h>
+#include <logger.h>
+
+#define TINYEXR_IMPLEMENTATION
+#include <tinyexr.h>
+
+namespace Caramel{
+
+    Image::Image(unsigned int width, unsigned int height)
+        : m_width(width), m_height(height) {
+        m_data.reserve(width*height*CHANNEL_NUM);
+    }
+
+    void Image::write_exr(const std::string &filename){
+
+        EXRHeader header;
+        InitEXRHeader(&header);
+
+        EXRImage image;
+        InitEXRImage(&image);
+
+        std::vector<Float> r(m_width*m_height);
+        std::vector<Float> g(m_width*m_height);
+        std::vector<Float> b(m_width*m_height);
+
+        for(int i=0;i<m_width*m_height;i++){
+            r[i] = m_data[i*CHANNEL_NUM+0];
+            g[i] = m_data[i*CHANNEL_NUM+1];
+            b[i] = m_data[i*CHANNEL_NUM+2];
+        }
+
+        Float *image_ptr[CHANNEL_NUM];
+        image_ptr[0] = &b[0];
+        image_ptr[1] = &g[0];
+        image_ptr[2] = &r[0];
+
+        image.images = (unsigned char**)image_ptr;
+        image.width = m_width;
+        image.height = m_height;
+
+        header.num_channels = CHANNEL_NUM;
+        header.channels = (EXRChannelInfo *)malloc(sizeof(EXRChannelInfo) * header.num_channels);
+        strncpy(header.channels[0].name, "B", 255); header.channels[0].name[strlen("B")] = '\0';
+        strncpy(header.channels[1].name, "G", 255); header.channels[1].name[strlen("G")] = '\0';
+        strncpy(header.channels[2].name, "R", 255); header.channels[2].name[strlen("R")] = '\0';
+
+        header.pixel_types = (int*)malloc(sizeof(int)*header.num_channels);
+        header.requested_pixel_types = (int *)malloc(sizeof(int) * header.num_channels);
+        for (int i = 0; i < header.num_channels; i++) {
+            // TINYEXR doesn't support double precision
+            header.pixel_types[i] = TINYEXR_PIXELTYPE_FLOAT;
+            header.requested_pixel_types[i] = TINYEXR_PIXELTYPE_FLOAT;
+        }
+
+        const char* err = nullptr;
+        int ret = SaveEXRImageToFile(&image, &header, filename.c_str(), &err);
+        if (ret != TINYEXR_SUCCESS) {
+            fprintf(stderr, "Save EXR err: %s\n", err);
+            MESSAGE(LogType::ERR, "Error occurs while save "+filename+" : "+err);
+            FreeEXRErrorMessage(err);
+        }
+        MESSAGE(LogType::LOG, "Saved exr file :"+filename);
+
+        free(header.channels);
+        free(header.pixel_types);
+        free(header.requested_pixel_types);
+    }
+
+    void Image::set_pixel_value(int w, int h, Float r, Float g, Float b) {
+        if(w<0 || m_width<=w || h<0 || m_height<=h){
+            MESSAGE(LogType::ERR, "unavailable image pixel position");
+        }
+
+        m_data[(w + h * m_width)*3] = r;
+        m_data[(w + h * m_width)*3 + 1] = g;
+        m_data[(w + h * m_width)*3 + 2] = b;
+
+    }
+
+}
