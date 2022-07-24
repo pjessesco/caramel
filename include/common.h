40e78775b03e48e05b2077bdd93bcc8279b15e99
Jino Park <pjessesco@gmail.com>
2022-07-24 23:19:20 +0900

    Add common.h

---
+++ include/common.h
@@ -0,0 +1,50 @@
+//
+// This software is released under the MIT license.
+//
+// Copyright (c) 2022 Jino Park
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+
+#pragma once
+
+#include <limits>
+
+namespace Caramel{
+    using Float = float;
+    using Int = int;
+    using Index = unsigned int;
+
+    constexpr Float INF = std::numeric_limits<Float>::infinity();
+
+    constexpr Float PI = 3.14159265359;
+    constexpr Float PI_2 = 6.28318530718;
+    constexpr Float PI_HALF = 1.57079632679;
+    constexpr Float PI_INV = 0.31830988618;
+    constexpr Float EPSILON = 1e-8;
+
+    inline Float deg_to_rad(Float deg){
+        return deg * PI / static_cast<Float>(180);
+    }
+
+    inline Float rad_to_deg(Float rad){
+        return rad * static_cast<Float>(180) * PI_INV;
+    }
+}
