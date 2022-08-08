3f9378762e7fdd8490f0bd2840959fc9b2ff11d5
Jino Park <pjessesco@gmail.com>
2022-08-08 19:45:44 +0900

    [WIP] Camera

---
+++ include/camera.h
@@ -0,0 +1,39 @@
+//
+// Created by Jino Park on 2022/08/08.
+//
+
+#pragma once
+
+#include <common.h>
+#include <ray.h>
+
+namespace Caramel{
+
+    // Perspective camera
+    struct Camera{
+        Camera(const Vector3f pos, const Vector3f dir, const Vector3f up,
+               Index w, Index h, Float near, Float far, Float fov_x)
+        : m_pos{pos}, m_dir{dir}, m_w{w}, m_h{h}, m_near{near}, m_far{far}, m_fov_x{fov_x} {
+            // right-handed coord
+            m_left = cross(up, dir);
+
+
+        }
+
+        Ray sample_ray(Float w, Float h){
+
+
+
+        }
+
+        Vector3f m_pos;
+        Vector3f m_dir;
+        Vector3f m_up;
+        Vector3f m_left;
+        Index m_w, m_h;
+        Float m_near, m_far;
+        Float m_fov_x;
+        Matrix44f m_cam_to_world;
+    };
+
+}
