5faffbf585aba050c0bc538903b1d2dabdfd737c
Jino Park <pjessesco@gmail.com>
2022-08-09 23:43:00 +0900

    Add cam

--- main.cpp
+++ main.cpp
@@ -2,20 +2,31 @@
 
 #include <logger.h>
 #include <shape.h>
+#include <camera.h>
 
 #include <Peanut/Peanut.h>
+
+using namespace Caramel;
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
 
-    Caramel::OBJMesh("/Users/jino/caramel/bunny.obj");
-    Caramel::OBJMesh("/Users/jino/caramel/ajax.obj");
+//    Caramel::OBJMesh("/Users/jino/caramel/bunny.obj");
+//    Caramel::OBJMesh("/Users/jino/caramel/ajax.obj");
 
-    WARNING("Warning example");
-    LOG("Log example");
-    ERROR("Error example");
+    Camera cam({-0.0315182f, 0.284011f, 0.7331f},
+               {-0.0191411f,  0.2299197f,  0.973022f,},
+               {0.00717446f, 0.973206f, -0.229822f},
+               40, 30, 45);
 
-    Peanut::Matrix<int, 2, 2> mat;
+
+    for(int i=0;i<40;i+=5){
+        for(int j=0;j<30;j+=5){
+            auto ray = cam.sample_ray(i, j);
+            std::cout<<"["<<ray.m_d[0]<<","<<ray.m_d[1]<<","<<ray.m_d[2]<<"],"<<std::endl;
+        }
+    }
+
 
     return 0;
 }
