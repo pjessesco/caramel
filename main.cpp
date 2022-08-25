02036a481055dfeef6a1647c4fa01323e6e16ca3
Jino Park <pjessesco@gmail.com>
2022-08-25 17:31:25 +0900

    Add bunny test case

--- main.cpp
+++ main.cpp
@@ -66,31 +66,55 @@ int main() {
 
     std::cout<<"===================================="<<std::endl;
 
+    // Test 3
     {
-        // Test 3
-        {
-            constexpr int TEST_W = 500;
-            constexpr int TEST_H = 500;
+        constexpr int TEST_W = 500;
+        constexpr int TEST_H = 500;
 
-            auto tri = Caramel::OBJMesh(test_scene_path + "case3/object.obj");
+        auto tri = Caramel::OBJMesh(test_scene_path + "case3/object.obj");
 
-            Camera cam({0.536078f, -0.536043f, -2.50343f},
-                       {-0.008999f, 0.009001f, 0.99992f},
-                       {8.1006e-05f, 0.99996f, -0.00900047f},
-                       TEST_W, TEST_H, 50);
+        Camera cam({0.536078f, -0.536043f, -2.50343f},
+                   {-0.008999f, 0.009001f, 0.99992f},
+                   {8.1006e-05f, 0.99996f, -0.00900047f},
+                   TEST_W, TEST_H, 50);
 
-            Image img(TEST_W, TEST_H);
+        Image img(TEST_W, TEST_H);
 
-            for(int i=0;i<TEST_W;i++){
-                for(int j=0;j<TEST_H;j++){
-                    auto ray = cam.sample_ray(i, j);
-                    auto [intersect, u, v, t] = tri.ray_intersect(ray);
-                    img.set_pixel_value(i, j, t, t, t);
-                }
+        for(int i=0;i<TEST_W;i++){
+            for(int j=0;j<TEST_H;j++){
+                auto ray = cam.sample_ray(i, j);
+                auto [intersect, u, v, t] = tri.ray_intersect(ray);
+                img.set_pixel_value(i, j, t, t, t);
             }
-
-            img.write_exr(test_scene_path+"case3/caramel_test3.exr");
         }
+
+        img.write_exr(test_scene_path+"case3/caramel_test3.exr");
+    }
+
+
+    // Test 4
+    {
+        constexpr int TEST_W = 100;
+        constexpr int TEST_H = 100;
+
+        auto tri = Caramel::OBJMesh(test_scene_path + "case4/bunny.obj");
+
+        Camera cam({-0.0315182f, 0.284011f, 0.7331f},
+                   {0.0191411f, -0.2299197f, -0.973022f},
+                   {0.00717446f, 0.973206f, -0.229822f},
+                   TEST_W, TEST_H, 16);
+
+        Image img(TEST_W, TEST_H);
+
+        for(int i=0;i<TEST_W;i++){
+            for(int j=0;j<TEST_H;j++){
+                auto ray = cam.sample_ray(i, j);
+                auto [intersect, u, v, t] = tri.ray_intersect(ray);
+                img.set_pixel_value(i, j, t, t, t);
+            }
+        }
+
+        img.write_exr(test_scene_path+"case4/caramel_test4.exr");
     }
 
     return 0;
