bd9984fff078a0cf48de76559d0013721e571679
Jino Park <pjessesco@gmail.com>
2022-08-30 17:05:41 +0900

    Fix integrator

--- include/integrators.h
+++ include/integrators.h
@@ -12,18 +12,8 @@ namespace Caramel{
     class Integrator{
     public:
         Integrator(const Scene &scene) : m_scene{scene} {}
-        virtual Image render() = 0;
 
-    protected:
-        Scene m_scene;
-    };
-
-
-    class DepthIntegrator : public Integrator{
-    public:
-        DepthIntegrator(const Scene &scene) : Integrator(scene) {}
-
-        Image render() override{
+        Image render(){
             const Index width = m_scene.m_cam.m_w;
             const Index height = m_scene.m_cam.m_h;
 
@@ -31,15 +21,26 @@ namespace Caramel{
 
             for(int i=0;i<width;i++){
                 for(int j=0;j<width;j++){
-                    const Ray ray = m_scene.m_cam.sample_ray(i, j);
-                    auto [is_hit, info] = m_scene.ray_intersect(ray);
-                    if(is_hit){
-                        img.set_pixel_value(i, j, info.t, info.t, info.t);
-                    }
+                    auto rgb = get_pixel_value(i, j);
+                    img.set_pixel_value(i, j, rgb[0], rgb[1], rgb[2]);
                 }
             }
-
             return img;
+        }
+
+    protected:
+        virtual Vector3f get_pixel_value(Float i, Float j) = 0;
+        Scene m_scene;
+    };
+
+    class DepthIntegrator : public Integrator{
+    public:
+        DepthIntegrator(const Scene &scene) : Integrator(scene) {}
+
+        Vector3f get_pixel_value(Caramel::Float i, Caramel::Float j) override{
+            const Ray ray = m_scene.m_cam.sample_ray(i, j);
+            auto [is_hit, info] = m_scene.ray_intersect(ray);
+            return is_hit ? Vector3f(info.t, info.t, info.t) : Vector3f();
         }
     };
 
@@ -47,23 +48,10 @@ namespace Caramel{
     public:
         UVIntegrator(const Scene &scene) : Integrator(scene) {}
 
-        Image render() override{
-            const Index width = m_scene.m_cam.m_w;
-            const Index height = m_scene.m_cam.m_h;
-
-            Image img(width, height);
-
-            for(int i=0;i<width;i++){
-                for(int j=0;j<width;j++){
-                    const Ray ray = m_scene.m_cam.sample_ray(i, j);
-                    auto [is_hit, info] = m_scene.ray_intersect(ray);
-                    if(is_hit){
-                        img.set_pixel_value(i, j, info.u, info.v, Float1 - info.u - info.v);
-                    }
-                }
-            }
-
-            return img;
+        Vector3f get_pixel_value(Caramel::Float i, Caramel::Float j) override{
+            const Ray ray = m_scene.m_cam.sample_ray(i, j);
+            auto [is_hit, info] = m_scene.ray_intersect(ray);
+            return is_hit ? Vector3f(info.u, info.v, Float1 - info.u - info.v) : Vector3f();
         }
     };
 }
