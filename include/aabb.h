fcc51aa36a4bac7ac36c4110e402ce6ebe047c74
Jino Park <pjessesco@gmail.com>
2022-09-15 01:27:59 +0900

    Split aabb declaration / definition
    wer

--- include/aabb.h
+++ include/aabb.h
@@ -6,49 +6,25 @@
 
 #include <cmath>
 
-#include "common.h"
-#include "logger.h"
-#include "ray.h"
+#include <common.h>
 
 namespace Caramel{
+
+    class Triangle;
+    class Ray;
+
     struct AABB {
-        AABB() {}
-        AABB(const Vector3f &p1, const Vector3f &p2) {
-            m_min = Vector3f{std::min({p1[0], p2[0]}),
-                             std::min({p1[1], p2[1]}),
-                             std::min({p1[2], p2[2]})};
+        AABB();
+        AABB(const Vector3f &p1, const Vector3f &p2);
 
-            m_max = Vector3f{std::max({p1[0], p2[0]}),
-                             std::max({p1[1], p2[1]}),
-                             std::max({p1[2], p2[2]})};
-        }
+        bool is_overlap(const AABB &aabb) const;
+        bool is_overlap(const Triangle &triangle) const;
 
-        bool is_overlap(const AABB &aabb){
-            return aabb.m_min[0] <= m_max[0] &&
-                   aabb.m_min[1] <= m_max[1] &&
-                   aabb.m_min[2] <= m_max[2] &&
-                   aabb.m_max[0] >= m_min[0] &&
-                   aabb.m_max[1] >= m_min[1] &&
-                   aabb.m_max[2] >= m_min[2];
-        }
+        bool is_contain(const Vector3f &vec) const;
 
-        Vector3f corner(Index i){
-            return Vector3f(i & 1 ? m_min[0] : m_max[0],
-                            i & 2 ? m_min[1] : m_max[1],
-                            i & 4 ? m_min[2] : m_max[2]);
-        }
+        Vector3f corner(Index i) const;
 
-        bool ray_intersect(const Ray &ray) const{
-            Float tmin = Float0;
-            Float tmax = INF;
-            for(Index i=0;i<3;i++){
-                Float t1 = (m_min[i] - ray.m_o[i]) / ray.m_d[i];
-                Float t2 = (m_max[i] - ray.m_o[i]) / ray.m_d[i];
-                tmin = std::min(std::max(t1, tmin), std::max(t2, tmin));
-                tmax = std::max(std::min(t1, tmax), std::min(t2, tmax));
-            }
-            return tmin <= tmax;
-        }
+        bool ray_intersect(const Ray &ray) const;
 
         Vector3f m_min;
         Vector3f m_max;
