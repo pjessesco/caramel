e6e6dcdccffd2b46fa14825470b378e791d14899
Jino Park <pjessesco@gmail.com>
2022-09-15 11:35:02 +0900

    Add test5

--- main.cpp
+++ main.cpp
@@ -108,7 +108,7 @@ int main() {
         Scene scene(Camera({-65.6055f, 47.5762f, 24.3583f},
                            {0.7894f, -0.3551f, -0.500699f},
                            {0.299858f, 0.934836f, -0.190177f},
-                           200, 200, 30));
+                           500, 500, 30));
 
         scene.add_mesh(new OBJMesh(test_scene_path + "case5/ajax.obj"));
 
