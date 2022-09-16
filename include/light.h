d230d141d5ea491ac5757bd991da6d286e79901d
Jino Park <pjessesco@gmail.com>
2022-09-17 00:59:26 +0900

    Implement area light
    .

--- include/light.h
+++ include/light.h
@@ -1,8 +1,56 @@
 //
-// Created by Jino on 2022/09/16.
+// This software is released under the MIT license.
+//
+// Copyright (c) 2022 Jino Park
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 //
 
-#ifndef CARAMEL_LIGHT_H
-#define CARAMEL_LIGHT_H
+#pragma once
 
-#endif //CARAMEL_LIGHT_H
+#include <tuple>
+
+#include <common.h>
+
+namespace Caramel{
+    class Scene;
+    class Shape;
+    class Sampler;
+
+    class Light{
+    public:
+        Light(const Scene &scene);
+        virtual Vector3f radiance() const = 0;
+        virtual std::tuple<Vector3f, Float> sample_contribution(const Vector3f &pos, Sampler &sampler) = 0;
+
+    protected:
+        const Scene &m_scene;
+    };
+
+    class AreaLight : public Light{
+    public:
+        AreaLight(const Scene &scene, std::shared_ptr<Shape> shape, const Vector3f &radiance);
+        Vector3f radiance() const override;
+        std::tuple<Vector3f, Float> sample_contribution(const Vector3f &hitpos, Sampler &sampler) override;
+
+        const std::weak_ptr<Shape> m_shape;
+        Vector3f m_radiance;
+    };
+
+}
---
+++ src/light.cpp
@@ -0,0 +1,69 @@
+//
+// This software is released under the MIT license.
+//
+// Copyright (c) 2022 Jino Park
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+#include <tuple>
+
+#include <common.h>
+#include <sampler.h>
+#include <shape.h>
+#include <scene.h>
+#include <light.h>
+
+namespace Caramel{
+
+    // -----------------------------------------------------------------
+
+    Light::Light(const Scene &scene) : m_scene{scene} {}
+
+    // -----------------------------------------------------------------
+
+    AreaLight::AreaLight(const Scene &scene, std::shared_ptr<Shape> shape, const Vector3f &radiance)
+        : Light(scene), m_shape{shape}, m_radiance{radiance} {
+        shape->set_light();
+    }
+
+    Vector3f AreaLight::radiance() const{
+        return m_radiance;
+    }
+
+    std::tuple<Vector3f, Float> AreaLight::sample_contribution(const Vector3f &hitpos, Sampler &sampler){
+        // Sample point on the shape
+        const auto [light_pos, light_normal_world, pos_pdf] = m_shape.lock()->sample_point(sampler);
+        const Vector3f light_to_hitpos = hitpos - light_pos;
+
+        // If hitpoint is behind of a sampled point, zero contribution
+        if(light_normal_world.dot(light_to_hitpos) <= 0){
+            return {Vector3f{Float0, Float0, Float0}, pos_pdf};
+        }
+
+        // If hitpoint and sampled point is not visible to each other, zero contribution
+        if(!m_scene.is_visible(hitpos, light_pos)){
+            return {Vector3f{Float0, Float0, Float0}, pos_pdf};
+        }
+
+        return {m_radiance, pos_pdf};
+    }
+
+
+}
