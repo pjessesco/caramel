c40ce8dd4d4315af73b6afc49ba285abab19ce2d
Jino Park <pjessesco@gmail.com>
2022-09-21 01:29:27 +0900

    Add test6 result

--- include/integrators.h
+++ include/integrators.h
@@ -40,7 +40,7 @@ namespace Caramel{
         Scene m_scene;
 
     private:
-        static constexpr Index SPP = 100;
+        static constexpr Index SPP = 2000;
     };
 
     class DepthIntegrator : public Integrator{
