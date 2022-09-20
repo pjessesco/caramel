14540fb4aa58005b1a0edc53baab74409f25bf56
Jino Park <pjessesco@gmail.com>
2022-09-20 11:18:39 +0900

    Split scene implementation into cpp file

--- src/scene.cpp
+++ src/scene.cpp
@@ -22,29 +22,58 @@
 // SOFTWARE.
 //
 
-#pragma once
-
 #include <memory>
 #include <tuple>
 
 #include <common.h>
 #include <ray.h>
 #include <shape.h>
-#include <transform.h>
 #include <camera.h>
 #include <light.h>
+#include <scene.h>
 
 namespace Caramel{
-    struct Scene{
-        explicit Scene(const Camera &cam);
 
-        std::tuple<bool, RayIntersectInfo> ray_intersect(const Ray &ray) const;
-        void add_mesh(const std::shared_ptr<Shape>& shape);
-        void add_light(const std::shared_ptr<Light>& light);
-        bool is_visible(const Vector3f pos1, const Vector3f &pos2) const;
+    Scene::Scene(const Camera &cam) : m_cam{cam} {}
 
-        std::vector<std::shared_ptr<Light>> m_lights;
-        std::vector<std::shared_ptr<Shape>> m_meshes;
-        Camera m_cam;
-    };
+    std::tuple<bool, RayIntersectInfo> Scene::ray_intersect(const Ray &ray) const{
+
+        bool is_hit = false;
+        RayIntersectInfo info = RayIntersectInfo();
+
+        for(int i=0;i<m_meshes.size();i++){
+            auto [hit, tmp_info] = m_meshes[i]->ray_intersect(ray);
+            if(hit){
+                is_hit = true;
+                if(info.t > tmp_info.t){
+                    info = tmp_info;
+                    info.idx = i;
+                }
+            }
+        }
+
+        return {is_hit, info};
+    }
+
+    void Scene::add_mesh(const std::shared_ptr<Shape>& shape){
+        m_meshes.emplace_back(shape);
+    }
+
+    void Scene::add_light(const std::shared_ptr<Light>& light){
+        m_lights.emplace_back(light);
+    }
+
+    bool Scene::is_visible(const Vector3f pos1, const Vector3f &pos2) const{
+        const Ray ray{pos1, pos2 - pos1};
+        const auto [is_hit, info] = ray_intersect(ray);
+
+        if(!is_hit){
+            return false;
+        }
+
+        const Vector3f dist = pos2 - pos1;
+
+        return dist.length() - info.t <= EPSILON;
+    }
+
 }
