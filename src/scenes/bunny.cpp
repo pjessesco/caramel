dcc65f49d772f617475798159966986bc25707fd
Jino Park <pjessesco@gmail.com>
2022-09-26 10:25:03 +0900

    Split scene-generate functions

---
+++ src/scenes/bunny.cpp
@@ -0,0 +1,22 @@
+//
+// Created by Jino Park on 2022/09/26.
+//
+
+#include <scene.h>
+#include <shape.h>
+#include <camera.h>
+#include <light.h>
+
+namespace Caramel{
+    Scene scene_bunny(){
+        std::string test_scene_path = "/Users/jino/caramel/test_scenes/";
+
+        Scene scene(Camera({-0.0315182f, 0.284011f, 0.7331f},
+                           {0.0191411f, -0.2299197f, -0.973022f},
+                           {0.00717446f, 0.973206f, -0.229822f},
+                           200, 200, 16));
+
+        scene.add_mesh(std::make_shared<OBJMesh>(test_scene_path + "case4/bunny.obj"));
+        return scene;
+    }
+}
