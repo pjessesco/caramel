38e9ec58a507e54a058b184444b94c01bad13487
Jino Park <pjessesco@gmail.com>
2022-09-26 20:24:57 +0900

    Implement brdf-sampling path tracing

--- include/integrators.h
+++ include/integrators.h
@@ -69,5 +69,16 @@ namespace Caramel{
         explicit DirectIntegrator(const Scene &scene);
         Vector3f get_pixel_value(Float i, Float j, Sampler &sampler) override;
     };
+
+    class PathIntegrator : public Integrator{
+    public:
+        explicit PathIntegrator(const Scene &scene, Index max_depth);
+        Vector3f get_pixel_value(Float i, Float j, Sampler &sampler) override;
+
+    private:
+        Vector3f brdf_sampling_path(Float i, Float j, Sampler &sampler);
+        Vector3f emitter_sampling_path(Float i, Float j, Sampler &sampler);
+        Index m_max_depth;
+    };
 }
 
