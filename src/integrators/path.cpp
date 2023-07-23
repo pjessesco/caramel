f48e5873efc1d5d86b8527722e4251c31d8a620f
Jino <pjessesco@gmail.com>
2023-07-23 16:17:51 +0900

    work?

--- src/integrators/path.cpp
+++ src/integrators/path.cpp
@@ -83,7 +83,6 @@ namespace Caramel{
 
     Vector3f PathIntegrator::emitter_sampling_path(const Scene &scene, Float i, Float j, Sampler &sampler){
         Ray ray = scene.m_cam->sample_ray(i, j);
-
         Vector3f current_brdf = vec3f_one;
         Vector3f ret = vec3f_zero;
         bool from_specular = true;
@@ -135,7 +134,66 @@ namespace Caramel{
 
 
     Vector3f PathIntegrator::mis_sampling_path(const Scene &scene, Float i, Float j, Sampler &sampler){
-        // WIP
-        return Vector3f();
+        Ray ray = scene.m_cam->sample_ray(i, j);
+        Vector3f current_brdf = vec3f_one;
+        Vector3f ret = vec3f_zero;
+        bool from_specular = true;
+        Float prev_brdf_pdf = Float1;
+
+        for(Index depth=0;depth<=m_max_depth;depth++){
+            auto [is_hit, info] = scene.ray_intersect(ray);
+
+            if(!is_hit){
+                break;
+            }
+
+            const Shape *shape = scene.m_meshes[info.idx];
+            const BSDF *shape_bsdf = shape->get_bsdf();
+            const Vector3f local_ray_dir = info.sh_coord.to_local(ray.m_d);
+
+            if(shape->is_light()){
+                const auto light = shape->get_arealight();
+                const Float pdf_solidangle = light->pdf_solidangle(ray.m_o, info.p, info.sh_coord.m_world_n);
+                const Float pdf_pick_light = Float1 / static_cast<Float>(scene.m_lights.size());
+                const Float weight = balance_heuristic(prev_brdf_pdf, pdf_pick_light * pdf_solidangle);
+                const Vector3f contrib = shape->get_arealight()->radiance() % current_brdf;
+                if(depth==0 || from_specular){
+                    ret = ret + contrib;
+                }
+                else{
+                    ret = ret + contrib * weight;
+                }
+                break;
+            }
+
+            if(depth == m_max_depth){
+                break;
+            }
+
+            // emiiter sampling
+            const bool is_current_specular = shape_bsdf->is_discrete();
+            if(!is_current_specular){
+                auto [light, light_pick_pdf] = scene.sample_light(sampler);
+                auto [emitted_rad, light_pos, light_n_world, light_pos_pdf, light_info] = light->sample_direct_contribution(scene, info.p, sampler);
+
+                const Vector3f hitpos_to_light_local_normal = info.sh_coord.to_local(light_pos - info.p).normalize();
+                const Vector3f fr = shape_bsdf->get_reflection(local_ray_dir, hitpos_to_light_local_normal);
+                const Float pdf_solidangle = light->pdf_solidangle(info.p, light_info.p, light_info.sh_coord.m_world_n);
+
+                // MIS for light sampling
+                const Float bsdf_pdf = shape->get_bsdf()->pdf(local_ray_dir, hitpos_to_light_local_normal);
+                const Float light_pdf = light_pick_pdf * pdf_solidangle;
+                ret = ret + (fr % emitted_rad % current_brdf) * std::abs(hitpos_to_light_local_normal[2]) * balance_heuristic(light_pdf, bsdf_pdf) / light_pdf;
+            }
+
+            /* brdf sampling */{
+                auto [local_recursive_dir, sampled_brdf, bsdf_pdf] = shape_bsdf->sample_recursive_dir(local_ray_dir, sampler);
+                current_brdf = current_brdf % sampled_brdf;
+                from_specular = is_current_specular;
+                ray = info.recursive_ray_to(local_recursive_dir);
+                prev_brdf_pdf = bsdf_pdf;
+            }
+        }
+        return ret;
     }
 }
