08df4a0821ff1fc62a8edd09baeeb46753316d9f
jino <pjessesco@gmail.com>
2023-08-05 17:39:47 +0900

    Implement MIS for path tracing (#15)

--- src/integrators/path.cpp
+++ src/integrators/path.cpp
@@ -57,7 +57,6 @@ namespace Caramel{
         Vector3f current_brdf = vec3f_one;
         Vector3f ret = vec3f_zero;
 
-        // Note the loop range difference
         for(Index depth=0;depth<=m_max_depth;depth++){
             auto [is_hit, info] = scene.ray_intersect(ray);
 
@@ -68,14 +67,24 @@ namespace Caramel{
             const Shape *shape = scene.m_meshes[info.idx];
 
             if(shape->is_light()){
-                return current_brdf % shape->get_arealight()->radiance();
+                return current_brdf % shape->get_arealight()->radiance(ray.m_o, info.p, info.sh_coord.m_world_n);
             }
 
-            // brdf sample
+            /* Russian roulette */{
+                if(depth >= m_rr_depth){
+                    if(current_brdf.max() > sampler.sample_1d()){
+                        current_brdf = current_brdf / current_brdf.max();
+                    }
+                    else{
+                        break;
+                    }
+                }
+            }
+
+            // brdf sampling
             const Vector3f local_ray_dir = info.sh_coord.to_local(ray.m_d);
             auto [local_recursive_dir, sampled_brdf, brdf_pdf] = shape->get_bsdf()->sample_recursive_dir(local_ray_dir, sampler);
             current_brdf = current_brdf % sampled_brdf;
-
             ray = info.recursive_ray_to(local_recursive_dir);
         }
 
@@ -84,12 +93,11 @@ namespace Caramel{
 
     Vector3f PathIntegrator::emitter_sampling_path(const Scene &scene, Float i, Float j, Sampler &sampler){
         Ray ray = scene.m_cam->sample_ray(i, j);
-
         Vector3f current_brdf = vec3f_one;
         Vector3f ret = vec3f_zero;
         bool from_specular = true;
 
-        for(Index depth=0;depth<m_max_depth;depth++){
+        for(Index depth=0;depth<=m_max_depth;depth++){
             auto [is_hit, info] = scene.ray_intersect(ray);
 
             if(!is_hit){
@@ -102,8 +110,12 @@ namespace Caramel{
 
             if(shape->is_light()){
                 if(depth == 0 || from_specular){
-                    ret = ret + (shape->get_arealight()->radiance() % current_brdf);
+                    ret = ret + (shape->get_arealight()->radiance(ray.m_o, info.p, info.sh_coord.m_world_n) % current_brdf);
                 }
+                break;
+            }
+
+            if(depth == m_max_depth){
                 break;
             }
 
@@ -113,32 +125,33 @@ namespace Caramel{
                 auto [light, light_pdf] = scene.sample_light(sampler);
                 auto [emitted_rad, light_pos, light_n_world, light_pos_pdf, light_info] = light->sample_direct_contribution(scene, info.p, sampler);
 
-                const Vector3f hitpos_to_light_local = info.sh_coord.to_local(light_pos - info.p).normalize();
-                const Float dist_square = light_info.t * light_info.t;
+                // Continue if light sampling succeed
+                if(!is_zero(emitted_rad)) {
+                    const Vector3f hitpos_to_light_local = info.sh_coord.to_local(light_pos - info.p).normalize();
+                    const Float dist_square = light_info.t * light_info.t;
+                    const Vector3f fr = shape_bsdf->get_reflection(local_ray_dir, hitpos_to_light_local);
+                    const Float geo = std::abs(info.sh_coord.to_local(light_n_world).dot(-hitpos_to_light_local) * hitpos_to_light_local[2]) / dist_square;
+                    const Float pdf = light_pdf * light_pos_pdf;
 
-                const Vector3f fr = shape_bsdf->get_reflection(local_ray_dir, hitpos_to_light_local);
+                    ret = ret + (fr % emitted_rad % current_brdf) * geo / pdf;
+                }
+            }
 
-                const Float geo = std::abs(info.sh_coord.to_local(light_n_world).dot(-hitpos_to_light_local) * hitpos_to_light_local[2]) / dist_square;
-                const Float pdf = light_pdf * light_pos_pdf;
-
-                ret = ret + (fr % emitted_rad % current_brdf) * geo / pdf;
+            /* Russian roulette */{
+                if(depth >= m_rr_depth){
+                    if(current_brdf.max() > sampler.sample_1d()){
+                        current_brdf = current_brdf / current_brdf.max();
+                    }
+                    else{
+                        break;
+                    }
+                }
             }
 
             // brdf sampling
             auto [local_recursive_dir, sampled_brdf, _] = shape_bsdf->sample_recursive_dir(local_ray_dir, sampler);
             current_brdf = current_brdf % sampled_brdf;
-            from_specular = shape_bsdf->is_discrete();
-
-            if(depth >= m_rr_depth){
-                const Float rr = current_brdf.max();
-                if(sampler.sample_1d() <= rr){
-                    current_brdf = current_brdf / rr;
-                }
-                else{
-                    break;
-                }
-            }
-
+            from_specular = is_current_specular;
             ray = info.recursive_ray_to(local_recursive_dir);
         }
         return ret;
@@ -146,7 +159,76 @@ namespace Caramel{
 
 
     Vector3f PathIntegrator::mis_sampling_path(const Scene &scene, Float i, Float j, Sampler &sampler){
-        // WIP
-        return Vector3f();
+        Ray ray = scene.m_cam->sample_ray(i, j);
+        Vector3f current_brdf = vec3f_one;
+        Vector3f ret = vec3f_zero;
+        bool from_specular = true;
+        Float prev_brdf_pdf = Float1;
+
+        for(Index depth=0;depth<=m_max_depth;depth++){
+            auto [is_hit, info] = scene.ray_intersect(ray);
+
+            if(!is_hit){
+                break;
+            }
+
+            const Shape *shape = scene.m_meshes[info.idx];
+            const BSDF *shape_bsdf = shape->get_bsdf();
+            const Vector3f local_ray_dir = info.sh_coord.to_local(ray.m_d);
+
+            if(shape->is_light()){
+                const auto light = shape->get_arealight();
+                const Float pdf_solidangle = light->pdf_solidangle(ray.m_o, info.p, info.sh_coord.m_world_n);
+                const Float pdf_pick_light = Float1 / static_cast<Float>(scene.m_lights.size());
+                const Float weight = balance_heuristic(prev_brdf_pdf, pdf_pick_light * pdf_solidangle);
+                const Vector3f contrib = shape->get_arealight()->radiance(ray.m_o, info.p, info.sh_coord.m_world_n) % current_brdf;
+
+                ret = ret + ((depth==0 || from_specular) ? contrib : contrib * weight);
+                break;
+            }
+
+            if(depth == m_max_depth){
+                break;
+            }
+
+            // emiiter sampling
+            const bool is_current_specular = shape_bsdf->is_discrete();
+            if(!is_current_specular){
+                auto [light, light_pick_pdf] = scene.sample_light(sampler);
+                auto [emitted_rad, light_pos, light_n_world, light_pos_pdf, light_info] = light->sample_direct_contribution(scene, info.p, sampler);
+
+                // Continue if light sampling succeed
+                if(!is_zero(emitted_rad)){
+                    const Vector3f hitpos_to_light_local_normal = info.sh_coord.to_local(light_pos - info.p).normalize();
+                    const Vector3f fr = shape_bsdf->get_reflection(local_ray_dir, hitpos_to_light_local_normal);
+                    const Float pdf_solidangle = light->pdf_solidangle(info.p, light_pos, light_info.sh_coord.m_world_n);
+
+                    // MIS for light sampling
+                    const Float bsdf_pdf = shape->get_bsdf()->pdf(local_ray_dir, hitpos_to_light_local_normal);
+                    const Float light_pdf = light_pick_pdf * pdf_solidangle;
+                    ret = ret + (fr % emitted_rad % current_brdf) * std::abs(hitpos_to_light_local_normal[2]) * balance_heuristic(light_pdf, bsdf_pdf) / light_pdf;
+                }
+            }
+
+            /* Russian roulette */{
+                if(depth >= m_rr_depth){
+                    if(current_brdf.max() > sampler.sample_1d()){
+                        current_brdf = current_brdf / current_brdf.max();
+                    }
+                    else{
+                        break;
+                    }
+                }
+            }
+
+            /* brdf sampling */{
+                auto [local_recursive_dir, sampled_brdf, bsdf_pdf] = shape_bsdf->sample_recursive_dir(local_ray_dir, sampler);
+                current_brdf = current_brdf % sampled_brdf;
+                from_specular = is_current_specular;
+                ray = info.recursive_ray_to(local_recursive_dir);
+                prev_brdf_pdf = bsdf_pdf;
+            }
+        }
+        return ret;
     }
 }
