40f3d32b0b28d91d8c8a9c8cb75e4f8a273b000c
jino <pjessesco@gmail.com>
2023-08-08 23:24:01 +0900

    Implement image texture (#17)

---
+++ src/textures/image_texture.cpp
@@ -0,0 +1,26 @@
+//
+// Created by Jino on 8/6/23.
+//
+
+#include <textures.h>
+#include <image.h>
+
+namespace Caramel{
+
+    ImageTexture::ImageTexture(const std::string &path)
+    : Texture() {
+        m_img = new Image(path);
+    }
+
+    ImageTexture::~ImageTexture(){
+        delete m_img;
+    }
+
+    Vector3f ImageTexture::get_val(const Vector2f &uv) const{
+
+        return m_img->get_pixel_value(m_img->m_width * uv[0],
+                                      m_img->m_height * (Float1 - uv[1]));
+    }
+
+
+}
